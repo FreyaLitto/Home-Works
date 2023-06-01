7520560e4b38cb278f40c5455a0210fb01a609d8
FreyaLitto <58091932+FreyaLitto@users.noreply.github.com>
2023-06-02 00:54:07 +0100

    Update program.cs, Word LOVE

--- program.cs
+++ program.cs
@@ -1,1 +1,14 @@
+using System.Runtime.CompilerServices;
 
+Console.WriteLine($"Домашне завдання #1");
+Console.WriteLine();
+
+Console.WriteLine("$$$_____$$$$$$$_$$$$$$$_$$$_______$$$_$$$$$$$$$$");
+Console.WriteLine("$$$____$$$____$$$____$$$_$$$_____$$$__$$$_______");
+Console.WriteLine("$$$____$$$_____$_____$$$_$$$_____$$$__$$$_______");
+Console.WriteLine("$$$_____$$$_________$$$___$$$___$$$___$$$$$$$$__");
+Console.WriteLine("$$$______$$$_______$$$_____$$$_$$$____$$$_______");
+Console.WriteLine("$$$_______$$$_____$$$______$$$_$$$____$$$_______");
+Console.WriteLine("$$$$$$$$$___$$$$$$$_________$$$$$_____$$$$$$$$$$");
+
+Console.ReadLine();
