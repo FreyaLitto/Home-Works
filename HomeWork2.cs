dcb39ff40e5df35101c476f53b104e1acd7a464d
FreyaLitto <58091932+FreyaLitto@users.noreply.github.com>
2023-06-12 01:19:24 +0100

    Add files via upload
    Signed-off-by: FreyaLitto <58091932+FreyaLitto@users.noreply.github.com>

---
+++ HomeWork2.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int val = 12345;
+int firstVal = val / 10000; //1
+int secondVal = val / 1000 - firstVal * 10; // 2
+int thirdVal = val / 100 - (firstVal * 100 + secondVal * 10); // 3
+int fourthVal = val / 10 - (firstVal * 1000 + secondVal * 100 + thirdVal * 10); // 4
+int fifthVal = val - (firstVal * 10000 + secondVal * 1000 + thirdVal * 100 + fourthVal * 10); // 5
+
+Console.WriteLine(firstVal);
+Console.WriteLine(secondVal);
+Console.WriteLine(thirdVal);
+Console.WriteLine(fourthVal);
+Console.WriteLine(fifthVal);
